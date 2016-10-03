749b0cfc1701050e53dccea1564ee9ae8d2f76ea
rahhal <saidbnrahhal@gmail.com>
2016-10-03 19:38:33 +0200

    general information

---
+++ maven/Tomcat/embaded tomcat/TP/test_tomcat_maven/src/main/java/com/said/rahhal/Controller.java
@@ -0,0 +1,33 @@
+package com.said.rahhal;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hellow")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public Controller() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
